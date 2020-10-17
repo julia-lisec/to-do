65985c95943062618295f75e3bfd14611fd27b92
Julia Lisec <julialisec12@gmail.com>
2020-10-17 11:46:38 +0200

    set back to default function

--- app.js
+++ app.js
@@ -48,6 +48,12 @@ function addItem(e) {
 
     // show container
     container.classList.add('show-container');
+
+    // add to local storage
+    addToLocalStorage(id, value);
+
+    // set back to default
+    setBackToDefault();
   }
   else if(value && editFlag) {
 
@@ -69,6 +75,17 @@ function displayAlert(text, action) {
   }, 1500)
 }
 
+// set back to default
+function setBackToDefault() {
+  grocery.value = '';
+  editFlag = false;
+  editID = '';
+  submitBtn.textContent = 'submit';
+}
+
 // ****** LOCAL STORAGE **********
+function addToLocalStorage(id, value) {
+  console.log('added to local storage');
+}
 
 // ****** SETUP ITEMS **********
